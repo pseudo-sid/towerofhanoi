34d3a5e91f812f532ebc9af73fd0bb8b48ee6ea2
Siddharth Singh <siddharth19111@iiitd.ac.in>
2020-09-14 17:47:25 +0530

    Comments added to move function

---
+++ towerofhanoi.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void move(int n, char source, char helper, char destination){
+	//base case
+
+
+	//recursive case
+}
+int main(int argc, char){
+	int n;	cin >> n;
+	
+	return 0;
+}

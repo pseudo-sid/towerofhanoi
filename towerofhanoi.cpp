bf4e54fae87285be5b346c52d3307cf471038c90
Siddharth Singh <siddharth19111@iiitd.ac.in>
2020-09-14 18:08:26 +0530

    Motion
    Building the recursive function for motion of disks and printing each motion.

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 void move(int n, char source, char helper, char destination){
 	//base case
-
+	if(n == 0)
+		return;	
 
 	//recursive case
+	//move n-1 disks from source to helper
+	move(n-1, source, destination, helper);
+	cout << "Shift disk " << n << " from " << source << " to " << destination << endl;
+	move(n-1, helper, source, destination);
+
 }
 int main(int argc, char){
-	int n;	cin >> n;
-	
+	int n;	cin >> n; // TAKES INPUT
+	move(n, 'A', 'B', 'C');
 	return 0;
 }
